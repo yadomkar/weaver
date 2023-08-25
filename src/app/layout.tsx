fbc3648d916d7e39caaa927af1a63618300600ac
Omkar Yadav <yadomkar@Omkars-MBP.attlocal.net>
2023-08-24 19:33:48 -0700

    auth set up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,38 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+
+import { Inter } from 'next/font/google';
+import { cn } from '@/lib/utils';
+import Navbar from '@/components/Navbar';
+import { Toaster } from '@/components/ui/toaster';
 
 export const metadata = {
   title: 'Breadit',
   description: 'A Reddit clone built with Next.js and TypeScript.',
-}
+};
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn(
+        'bg-white text-slate-900 antialiased light',
+        inter.className
+      )}
+    >
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        <Navbar />
+        <div className="container max-w-7xl mx-aut h-full pt-22">
+          {children}
+        </div>
+
+        <Toaster />
+      </body>
     </html>
-  )
+  );
 }
---
+++ src/components/UserAccountNav.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+interface UserAccountNavProps {}
+
+const UserAccountNav: FC<UserAccountNavProps> = ({}) => {
+  return <div>UserAccountNav</div>;
+};
+
+export default UserAccountNav;
